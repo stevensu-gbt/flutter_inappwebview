117d9420b6fea6588988cd59607a1d1e9054e1a1
stevensu-gbt <steven.su@shechemtech.com>
2026-07-17 16:02:11 +0800

    feat(android): export profile-aware WebView widget

--- flutter_inappwebview_android/lib/src/in_app_webview/main.dart
+++ flutter_inappwebview_android/lib/src/in_app_webview/main.dart
@@ -1,3 +1,4 @@
 export 'in_app_webview_controller.dart' hide InternalInAppWebViewController;
 export 'in_app_webview.dart';
+export 'android_profile_in_app_webview.dart';
 export 'headless_in_app_webview.dart' hide InternalHeadlessInAppWebView;
